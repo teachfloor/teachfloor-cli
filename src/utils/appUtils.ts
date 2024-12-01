7a37f5d77160910ff44ca04b00cd8a49008722a1
Stefano Marra <stefano.marra1987@gmail.com>
2024-12-01 14:05:39 +0100

    Add extension kit package

--- src/utils/appUtils.ts
+++ src/utils/appUtils.ts
@@ -1,22 +1,108 @@
-import * as fs from 'fs';
+import * as fs from 'fs'
+
+const getSDKSnippet = (id: string) => (
+  `<!-- Teachfloor SDK -->
+    <script>
+        (function (t, c, h, f, l, r) {
+            t.tf = t.tf || function () { (t.tf.q = t.tf.q || []).push(arguments) }
+            t._tfOpts = { id: '${id}' }
+            l = c.getElementsByTagName('head')[0];
+            r = c.createElement('script');
+            r.async = 1;
+            r.src = h + t._tfOpts.id + f + Date.now();
+            l.appendChild(r)
+        })(window, document, 'https://${process.env.API_URL || 'app.teachfloor.com'}/apps/', '.js?ver=')
+    </script>
+    <!-- End Teachfloor SDK -->`
+)
+
+const getIndexHtmlSnippet = (appName: string, sdkSnippet: string) => (
+`<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>${appName}</title>
+  ${sdkSnippet}
+</head>
+<body>
+  <div id="root"></div>
+</body>
+</html>`
+)
+
+const getWebpackConfigSnippet = () => (
+`const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    port: ${parseInt(process.env.PORT || '3000', 10)},
+    open: true,
+    hot: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'public/index.html', // Path to your template
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+};`
+)
+
+const getExampleViewSnippet = () => (
+`import React, { useEffect } from 'react';
+import { Container, Text } from '@teachfloor/extension-kit/ui'
+
+const App = () => {
+  useEffect(() => {
+    tf('onInit', function (API) {
+      API.on('environment.viewport.changed', (viewport, objectContext) => {
+        console.log('SDK from CLI viewport', viewport, objectContext)
+      });
+    })
+  },[])
+
+  return (
+    <Container>
+      <Text>Welcome to Hello World!</Text>
+    </Container>
+  );
+};
+
+export default App;
+`
+)
 
 export const createAppStructure = (rootDir: string, appId: string, appName: string, description: string, version: string) => {
   /**
    * TODO - Get the SDK snippet from api
    */
-  const sdkSnippet = `<!-- Teachfloor SDK -->
-  <script>
-      (function (t, c, h, f, l, r) {
-          t.tf = t.tf || function () { (t.tf.q = t.tf.q || []).push(arguments) }
-          t._tfOpts = { id: '674b222f3a113' }
-          l = c.getElementsByTagName('head')[0];
-          r = c.createElement('script');
-          r.async = 1;
-          r.src = h + t._tfOpts.id + f + Date.now();
-          l.appendChild(r)
-      })(window, document, 'https://app.teachfloor.com/apps/', '.js?ver=')
-  </script>
-  <!-- End Teachfloor SDK -->`
+  const sdkSnippet = getSDKSnippet('674b222f3a113')
 
   /**
    * App folders that will be created
@@ -44,23 +130,7 @@ ReactDOM.render(<App />, document.getElementById('root'));
     /**
      * View example
      */
-    [`${rootDir}/src/views/App.tsx`]:
-`import React, { useEffect } from 'react';
-
-const App = () => {
-  useEffect(() => {
-    tf('onInit', function (API) {
-      API.on('environment.viewport.changed', (viewport, objectContext) => {
-        console.log('SDK from CLI viewport', viewport, objectContext)
-      });
-    })
-  },[])
-
-  return <div>Welcome to ${appName}!</div>;
-};
-
-export default App;
-`,
+    [`${rootDir}/src/views/App.jsx`]: getExampleViewSnippet(),
 
     // [`${rootDir}/src/views/App.test.tsx`]: `
     //   import { render } from '@testing-library/react';
@@ -105,13 +175,14 @@ export default App;
         '@babel/preset-react': '^7.18.0',
         'ts-loader': '^9.0.0',
         'typescript': '^4.0.0',
-        '@types/react': '^17.0.0',
-        '@types/react-dom': '^17.0.0',
-        "html-webpack-plugin": "^5.5.3"
+        '@types/react': '^18.0.0',
+        '@types/react-dom': '^18.0.0',
+        "html-webpack-plugin": "^5.5.3",
       },
       dependencies: {
-        react: '^17.0.0',
-        'react-dom': '^17.0.0',
+        react: '^18.0.0',
+        'react-dom': '^18.0.0',
+        '@teachfloor/extension-kit': `^${process.env.EXTENSION_KIT_VERSION || '1.0.3'}`,
       },
       babel: {
         presets: [
@@ -124,66 +195,12 @@ export default App;
     /**
      * public/index.html
      */
-    [`${rootDir}/public/index.html`]:
-`<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>${appName}</title>
-  ${sdkSnippet}
-</head>
-<body>
-  <div id="root"></div>
-</body>
-</html>
-    `,
+    [`${rootDir}/public/index.html`]: getIndexHtmlSnippet(appName, sdkSnippet),
 
     /**
      * webpack.config.js
      */
-    [`${rootDir}/webpack.config.js`]:
-`const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  devServer: {
-    static: {
-      directory: path.join(__dirname, 'public'),
-    },
-    port: ${parseInt(process.env.PORT || '3000', 10)},
-    open: true,
-    hot: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: 'public/index.html', // Path to your template
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.jsx?$/,
-        use: 'babel-loader',
-        exclude: /node_modules/,
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.jsx'],
-  },
-};
-    `,
+    [`${rootDir}/webpack.config.js`]: getWebpackConfigSnippet(),
 
     [`${rootDir}/tsconfig.json`]: JSON.stringify({
       compilerOptions: {
@@ -237,4 +254,4 @@ ${description}
   Object.entries(files).forEach(([filePath, content]) => {
     fs.writeFileSync(filePath, content);
   });
-};
+}
